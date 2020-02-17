f09d9b76f55fd3f7208813f4bb494d81dfbd1fa1
Khánh Nguyễn <40472534+khanhnguyen1999@users.noreply.github.com>
2020-02-18 01:27:35 +0700

    toggle form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,26 +1,38 @@
-import React from 'react';
-export default function AddTask(){
-    return(
-        <div className="col-12 col-lg-6">
-          <div className="form-group add-task">
-            <button type="button" className="btn btn-info btn-block">Add Task</button>
-          </div>
-          <form action="" method="POST" className="form-inline justify-content-between">
-            <div className="form-group">
-              <label className="sr-only" for="">label</label>
-              <input type="text" className="form-control" placeholder="Task Name" />
-            </div>
-            <div className="form-group">
-              <label className="sr-only" for="">label</label>
-              <select name="ds" className="form-control" required="required">
-                <option value="0">Small</option>
-                <option value="1">Medium</option>
-                <option value="2">High</option>
-              </select>
-            </div>
-            <button type="button" className="btn btn-primary">Submit</button>
-            <button type="button" className="btn btn-secondary">Cancel</button>
-          </form>
-        </div>
-    )
+import React,{Component} from 'react';
+class AddTask extends Component{
+    constructor (props)
+    {
+        super(props);
+        this.state={
+            isSwitch:false
+        }
     }
+    render(){
+        return(
+            <div className="col-12 col-lg-6">
+            <div className="form-group add-task">
+                <button type="button" className={this.state.isSwitch?"btn btn-block btn-primary":"btn btn-block btn-info"}
+                    onClick={()=>this.setState({isSwitch:!this.state.isSwitch})}
+                >{this.state.isSwitch?'Close Form':'Add Task'}</button>
+            </div>
+            <form style={this.state.isSwitch?{opacity:'1'}:{opacity:'0'}} action="" method="POST" className="form-inline justify-content-between">
+                <div className="form-group">
+                <label className="sr-only" for="">label</label>
+                <input type="text" className="form-control" placeholder="Task Name" />
+                </div>
+                <div className="form-group">
+                <label className="sr-only" for="">label</label>
+                <select name="ds" className="form-control" required="required">
+                    <option value="0">Small</option>
+                    <option value="1">Medium</option>
+                    <option value="2">High</option>
+                </select>
+                </div>
+                <button type="button" className="btn btn-primary">Submit</button>
+                <button type="button" className="btn btn-secondary">Cancel</button>
+            </form>
+            </div>
+        )
+        }
+    }
+export default AddTask;
--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -16,7 +16,7 @@ export default function ListTask(){
               <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
               <td className="text-center"><span className="badge badge-danger">High</span></td>
               <td>
-                <button type="button" className="btn btn-warning">Edit</button>
+                <button type="button" className="mr-1 btn btn-warning">Edit</button>
                 <button type="button" className="btn btn-danger">Delete</button>
               </td>
             </tr>
@@ -25,7 +25,7 @@ export default function ListTask(){
               <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
               <td className="text-center"><span className="badge badge-secondary">Small</span></td>
               <td>
-                <button type="button" className="btn btn-warning">Edit</button>
+                <button type="button" className="mr-1 btn btn-warning">Edit</button>
                 <button type="button" className="btn btn-danger">Delete</button>
               </td>
             </tr>
@@ -34,7 +34,7 @@ export default function ListTask(){
               <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
               <td className="text-center"><span className="badge badge-info">Medium</span></td>
               <td>
-                <button type="button" className="btn btn-warning">Edit</button>
+                <button type="button" className="mr-1 btn btn-warning">Edit</button>
                 <button type="button" className="btn btn-danger">Delete</button>
               </td>
             </tr>
